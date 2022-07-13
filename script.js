14796ba4b38b01c98907f3c1ada35d3d4d8b7285
Yurii Lukianovych <38596364+YuriiLukianovych@users.noreply.github.com>
2022-07-13 19:18:21 +0300

    wergewger

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ const popupWrapper = document.querySelector(".popup-wrapper");
 const popupBody = document.querySelector(".popup");
 const btn = document.querySelector(".btn");
 const closeBtn = document.querySelector(".close-btn");
+const leftSidePopup = document.querySelector(".left-side");
+const rightSidePopup = document.querySelector(".right-side");
 
 btn.addEventListener("click", () => {
     popupWrapper.classList.add("is-visible");
     popupBody.classList.add("is-visible");
+    leftSidePopup.classList.add("is-visible");
+    rightSidePopup.classList.add("is-visible");
     mainContent.classList.add("is-hide");
+    closeBtn.classList.add("is-visible");
 });
 
 closeBtn.addEventListener("click", () => {
     popupWrapper.classList.remove("is-visible");
     popupBody.classList.remove("is-visible");
+    leftSidePopup.classList.remove("is-visible");
+    rightSidePopup.classList.remove("is-visible");
     mainContent.classList.remove("is-hide");
+    closeBtn.classList.remove("is-visible");
 });
